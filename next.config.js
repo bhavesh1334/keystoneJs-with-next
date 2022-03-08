efd7f7c37257281cddf66229cbe12702a1437986
bhavesh-chandrakar <bhavesh1334@gmail.com>
2022-03-08 12:57:23 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+
+
+ const { withKeystone } = require("@keystone-6/core/next");
+ module.exports = withKeystone({
+  reactStrictMode: true,
+});
+
+
+
+// module.exports = nextConfig
